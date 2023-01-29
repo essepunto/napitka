9ccb6552103ceee5a16fc47bcbf0328e2039181f
Alex <Alex@DESKTOP-OVCFLFS>
2023-01-29 13:20:39 +0300

    доработка

--- app/src/main/java/ru/essepunto/napitka/MainActivity.java
+++ app/src/main/java/ru/essepunto/napitka/MainActivity.java
@@ -2,8 +2,6 @@ package ru.essepunto.napitka;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
